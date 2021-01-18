1529daf3ff5c9ec56c563227300af56263304d87
Marcus Isaksson <me@naknut.email>
2021-01-17 18:04:27 -0800

    Now uses google types

--- smart-light-server/src/main.rs
+++ smart-light-server/src/main.rs
@@ -1,40 +1,16 @@
-use tonic::{transport::Server, Request, Response, Status};
-
-use smart_light::light_server::{Light, LightServer};
-use smart_light::{Empty, BoolValue};
-use std::sync::{Mutex};
-
+use tonic::transport::{Server};
 use rppal::gpio::Gpio;
-use rppal::gpio::{Level, OutputPin};
-
-pub mod smart_light {
-    tonic::include_proto!("smartlight");
-}
-
-#[derive(Debug)]
-pub struct Output {
-    pin: Mutex<OutputPin>
-}
-
-#[tonic::async_trait]
-impl Light for Output {
-    async fn is_on(&self, _request: Request<Empty>) -> Result<Response<BoolValue>, Status> {
-        Ok(Response::new(BoolValue { value: self.pin.lock().unwrap().is_set_high() }))
-    }
-
-    async fn set_is_on(&self, request: Request<BoolValue>) -> Result<Response<Empty>, Status> {
-        self.pin.lock().unwrap().write(if request.into_inner().value { Level::High } else { Level::Low });
-        Ok(Response::new(Empty {}))
-    }
-}
+use std::sync::{Mutex};
+mod light_service;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let addr = "0.0.0.0:50051".parse()?;
+    let pin = Mutex::new(Gpio::new()?.get(18)?.into_output());
 
     println!("Listening on {}", addr);
     Server::builder()
-        .add_service(LightServer::new(Output { pin: Mutex::new(Gpio::new()?.get(18)?.into_output()) }))
+        .add_service(light_service::server(pin))
         .serve(addr)
         .await?;
 
