4e11d15e09c89459b5b33234b6a32cc5bc195eb3
Marcus Isaksson <me@naknut.email>
2021-01-20 20:54:15 -0800

    Add Apple Watch app and fix macOS app

---
+++ SmartLight/SmartLight Extension/SmartLightApp.swift
@@ -0,0 +1,19 @@
+//
+//  SmartLightApp.swift
+//  SmartLight Extension
+//
+//  Created by Marcus Isaksson on 1/20/21.
+//
+
+import SwiftUI
+
+@main
+struct SmartLightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
